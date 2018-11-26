31b1ebea2a475ef08e6b032bff03821d69969e73
chase-stevens <chasestevenspersonal@gmail.com>
2018-11-26 09:19:39 -0700

    interval passes into listener object

--- app/controllers/listeners_controller.rb
+++ app/controllers/listeners_controller.rb
@@ -1,8 +1,20 @@
 class ListenersController < ApplicationController
+    def show
+        @listener = Listener.find(params[:id])
+    end
+
     def new
     end
 
     def create
-        render plain: params[:listener].inspect
+        @listener = Listener.new(listener_params)
+
+        @listener.save
+        redirect_to @listener
     end
+
+    private
+        def listener_params
+            params.require(:listener).permit(:title, :interval)
+        end
 end
