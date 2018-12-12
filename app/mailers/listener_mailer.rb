2c09dfde0ebcc8a64ef99eac3b3bde9431874053
chase-stevens <chasestevenspersonal@gmail.com>
2018-12-11 17:58:33 -0700

    added unique email verification for users

--- app/mailers/listener_mailer.rb
+++ app/mailers/listener_mailer.rb
@@ -1,6 +1,7 @@
 class ListenerMailer < ApplicationMailer
     default from: 'notifications@listener.com' # placeholder
     def failure_email
+        @user = params[:user]
         @listener = params[:listener]
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_001316) do
+ActiveRecord::Schema.define(version: 2018_12_12_005639) do
 
   create_table "checkins", force: :cascade do |t|
     t.text "note"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_001316) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
