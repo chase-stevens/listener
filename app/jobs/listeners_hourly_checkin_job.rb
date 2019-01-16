63ce53f207fa2808a0d472afdbfe405b46e54dbf
chase-stevens <chasestevenspersonal@gmail.com>
2019-01-16 15:58:15 -0700

    updated checkin job

--- app/jobs/listeners_hourly_checkin_job.rb
+++ app/jobs/listeners_hourly_checkin_job.rb
@@ -3,19 +3,15 @@ require 'date'
 class ListenersHourlyCheckinJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(*listeners)
     current_period = DateTime.now.beginning_of_hour
-    Lisener.all.each do |listener|
+
+    listeners.each do |listener|
       if listener.checkins.order("created_at").last >= current_period && listener.checked_in == true
         listener.checked_in = false
       else
-        # mailer does not currently exist
-        ListenerMailer.with(listener.email).failure_email.deliver_now 
-    # listeners.each do |listener|
-    # => if listener.checkin.last.created_at >= current_period - 1
-    # =>  listener.checked_in = false
-    # => else
-    # =>  mail failure_email to listener.email
-    # => end
+        ListenerMailer.with(listener.email).failure_email.deliver_now
+      end
+    end
   end
 end
